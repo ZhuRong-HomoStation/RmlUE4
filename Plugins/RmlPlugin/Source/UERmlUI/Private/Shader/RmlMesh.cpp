54da5cea06c6ce957c234873a8b54e4500855956
zhurongjun <211103376@qq.com>
2021-01-03 16:12:46 +0800

    Mesh上线

---
+++ Plugins/RmlPlugin/Source/UERmlUI/Private/Shader/RmlMesh.cpp
@@ -0,0 +1,36 @@
+﻿#include "RmlMesh.h"
+
+void FRmlMesh::BuildMesh()
+{
+	if (Vertices.Num() == 0 || Indices.Num() == 0) return;
+	if (IsInRenderingThread())
+	{
+		BuildMesh_RenderThread();
+	}
+	else
+	{
+		ENQUEUE_RENDER_COMMAND(SetupOutputRemapMeshCmd)([this](FRHICommandListImmediate& RHICmdList)
+        {
+            BuildMesh_RenderThread();
+        });
+	}
+}
+
+void FRmlMesh::ReleaseMesh()
+{
+	VertexBufferRHI.SafeRelease();
+	IndexBufferRHI.SafeRelease();
+}
+
+void FRmlMesh::BuildMesh_RenderThread()
+{
+	FRHIResourceCreateInfo VtxInfo;
+	VtxInfo.ResourceArray = &Vertices;
+	int32 VerticesBufferSize = sizeof(FVertexData) * Vertices.Num();
+	VertexBufferRHI = RHICreateVertexBuffer(VerticesBufferSize, BUF_Static, VtxInfo);
+
+	FRHIResourceCreateInfo IdxInfo;
+	IdxInfo.ResourceArray = &Indices;
+	int32 IndexBufferSize = sizeof(uint16) * Indices.Num();
+	IndexBufferRHI = RHICreateIndexBuffer(IndexBufferSize, 0, BUF_Static, IdxInfo);
+}
---
+++ Plugins/RmlPlugin/Source/UERmlUI/Private/Shader/RmlMesh.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+class FRmlMesh
+{
+public:
+	struct FVertexData
+	{
+		FVector2D	Position;
+		FVector2D	UV;
+		FVector4	Color;
+		FVertexData(const FVector2D& InPos, const FVector2D& InUV, const FVector4& InColor)
+			: Position(InPos), UV(InUV), Color(InColor)
+		{}
+	};
+
+	void BuildMesh();
+	void ReleaseMesh();
+private:
+	void BuildMesh_RenderThread();
+
+public:
+	TResourceArray<FVertexData>		Vertices;
+	FVertexBufferRHIRef				VertexBufferRHI;
+
+	TResourceArray<uint16>			Indices;
+	FIndexBufferRHIRef				IndexBufferRHI;
+};
